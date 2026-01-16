24504aab1545249f0d19705ea40529d1394b16ee
Nigam Subedi <nigam333nigam@gmail.com>
2026-01-16 22:02:55 +0545

    fixed profile modal by breaking into components

--- frontend/src/pages/Home/Profile/ModalFooter.jsx
+++ frontend/src/pages/Home/Profile/ModalFooter.jsx
@@ -2,7 +2,7 @@ import { Save } from "lucide-react";
 
 export default function ModalFooter({ onCancel, onSave }) {
   return (
-    <div className="p-6 border-t border-gray-200/30 bg-gradient-to-r from-gray-50/50 to-white/50">
+    <div className="p-6 border-t border-gray-200/30 bg-linear-to-r from-gray-50/50 to-white/50">
       <div className="flex justify-end gap-3">
         <button
           onClick={onCancel}
@@ -13,7 +13,7 @@ export default function ModalFooter({ onCancel, onSave }) {
 
         <button
           onClick={onSave}
-          className="px-6 py-3 text-sm font-medium text-white bg-gradient-to-b from-blue-600 to-blue-400 rounded-xl shadow flex items-center gap-2"
+          className="px-6 py-3 text-sm font-medium text-white bg-linear-to-b from-blue-600 to-blue-400 rounded-xl shadow flex items-center gap-2"
         >
           <Save className="w-4 h-4" />
           Save Changes
--- frontend/src/pages/Home/Profile/ToggleSwitch.jsx
+++ frontend/src/pages/Home/Profile/ToggleSwitch.jsx
@@ -7,7 +7,7 @@ export default function ToggleSwitch({ checked, onChange }) {
         onChange={(e) => onChange(e.target.checked)}
         className="sr-only peer"
       />
-      <div className="relative w-11 h-6 bg-gray-300 rounded-full peer-checked:bg-blue-600 after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:h-5 after:w-5 after:bg-white after:rounded-full after:transition-transform after:duration-300 peer-checked:after:translate-x-5" />
+      <div className="relative w-11 h-6 bg-gray-300 rounded-full peer-checked:bg-blue-600 after:content-[''] after:absolute after:top-0.5 after:left-0.5 after:h-5 after:w-5 after:bg-white after:rounded-full after:transition-transform after:duration-300 peer-checked:after:translate-x-5" />
     </label>
   );
 }
